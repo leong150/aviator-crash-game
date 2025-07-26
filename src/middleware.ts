97f2026170a3d12d26b9a7b83cf8c419f00bf5c1
leong150 <leongyongsheng@hotmail.com>
2025-07-26 15:00:47 +0800

    setup middleware

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const middleware = (request: NextRequest) => {
+  const { pathname } = request?.nextUrl || {};
+
+  if (pathname !== `/gameplay`) {
+    return NextResponse.redirect(new URL(`/gameplay`, request.url));
+  }
+
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    `/((?!api|_next/static|_next/image).*)`,
+  ],
+};
+
+export default middleware;
